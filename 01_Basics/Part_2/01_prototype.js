2f3c261557dd1fd8022c724f0d1e18b92e0d7d0a
Anup Chauhan <84135396+anupchauhanak6@users.noreply.github.com>
2025-12-24 20:04:59 +0530

    refactor: clean up prototype.js and enhance readability

--- 01_Basics/Part_2/01_prototype.js
+++ 01_Basics/Part_2/01_prototype.js
@@ -1,3 +1,5 @@
+//prototype in js
+
 let computer = {cpu: 'i7', ram: '16GB'};
 
 let lenevo = {
@@ -7,4 +9,15 @@ let lenevo = {
 
 let dell = {}
 
-console.log(lenevo,lenevo.__proto__);
+// console.log(lenevo,lenevo.__proto__);
+
+
+let car = {tyres : "4"}
+
+let tesla = {
+    driver: 'AI'
+}
+
+Object.setPrototypeOf(tesla, car)
+
+console.log("tesla", tesla, ', and tyre:', tesla.tyres, ',', Object.getPrototypeOf(tesla)); //different methods
